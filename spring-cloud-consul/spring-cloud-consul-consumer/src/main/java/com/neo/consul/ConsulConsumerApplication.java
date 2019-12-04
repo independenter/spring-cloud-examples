03f8bec116ce7e3e824508a85b2c2d4ee649e43d
donghui <15501951002@163.com>
2019-12-04 21:33:27 +0800

    修改多模块

--- spring-cloud-consul/spring-cloud-consul-consumer/src/main/java/com/neo/consul/ConsulConsumerApplication.java
+++ spring-cloud-consul/spring-cloud-consul-consumer/src/main/java/com/neo/consul/ConsulConsumerApplication.java
@@ -2,6 +2,7 @@ package com.neo.consul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class ConsulConsumerApplication {
